5a80bfe511b7f5f0ddc01bd0d636b75578bd8fcd
Paweł Niedźwiecki <hello@uxu.pl>
2023-10-05 07:26:10 +0200

    fix: turbo.json

--- apps/wtrasie/src/config/apolloClient/config.links.ts
+++ apps/wtrasie/src/config/apolloClient/config.links.ts
@@ -2,7 +2,7 @@ import { HttpLink } from '@apollo/client';
 import { RestLink } from 'apollo-link-rest';
 
 
-export const LINK_API_GRAPHQL = new HttpLink({ uri: process.env.NEXT_PUBLIC_LINK_API_GRAPHQL });
+export const LINK_API_GRAPHQL = new HttpLink({ uri: "https://wtrasie.herokuapp.com/graphql" });
 export const LINK_API_REST = new RestLink({
-  uri: process.env.NEXT_PUBLIC_LINK_API_REST
+  uri: "https://wtrasie.herokuapp.com/api/"
 });
