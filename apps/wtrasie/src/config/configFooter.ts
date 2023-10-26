8aae3afd00c5cf49d4f47d522427b0a45946c9a1
Paweł Niedźwiecki <hello@uxu.pl>
2023-10-26 11:41:33 +0200

    fix: slug

--- apps/wtrasie/src/config/configFooter.ts
+++ apps/wtrasie/src/config/configFooter.ts
@@ -10,10 +10,149 @@ type FooterConfigTypes = {
 export const FOOTER_CONFIG: FooterConfigTypes = {
   footer: {
     mobile: {
-      footerColumns: [],
+      footerColumns: [
+        {
+          header: "Przydatne",
+          id: "useful",
+          links: [
+            {
+              id: 'about',
+              title: "O nas",
+              linkPath: "/"
+            },
+            {
+              id: 'blog',
+              title: "Blog",
+              linkPath: "/blog"
+            },
+          ]
+        },
+        {
+          header: "Dla klienta",
+          id: "forClient",
+          links: [
+            {
+              id: 'whatItsWork',
+              title: "Jak to działa ?",
+              linkPath: "/"
+            },
+            {
+              id: 'faq',
+              title: "FAQ",
+              linkPath: "/"
+            },
+            {
+              id: 'contact',
+              title: "Kontakt",
+              linkPath: "/"
+            }
+          ]
+        },
+        {
+          header: "Nasze serwisy",
+          id: "ourWebSite",
+          links: [
+            {
+              id: 'uxu',
+              title: "uxu.pl",
+              linkPath: "https://www.uxu.pl",
+            },
+            {
+              id: 'polskidev',
+              title: "polski.dev",
+              linkPath: "https://www.polski.dev",
+            },
+            {
+              id: 'wTrasie',
+              title: "wtrasie.pl",
+              linkPath: "https://www.wtrasie.pl",
+            },
+          ]
+        },
+        {
+          header: "Informacje prawne",
+          id: "lawsInformation",
+          links: [
+            {
+              id: 'statute',
+              title: "Regulamin",
+              linkPath: "/"
+            },
+            {
+              id: 'privacyPolicy',
+              title: "Polityka Prywatności",
+              linkPath: "/"
+            }
+          ]
+        }
+      ],
     },
     desktop: {
-      footerColumns: [],
+      footerColumns: [
+        {
+          header: "Przydatne",
+          id: "useful",
+          links: [
+            {
+              id: 'about',
+              title: "O nas",
+              linkPath: "/"
+            },
+            {
+              id: 'blog',
+              title: "Blog",
+              linkPath: "/blog"
+            },
+            {
+              id: 'faq',
+              title: "Faq",
+              linkPath: "/"
+            },
+            {
+              id: 'contact',
+              title: "Kontakt",
+              linkPath: "/"
+            }
+          ]
+        },
+        {
+          header: "Nasze serwisy",
+          id: "ourWebSite",
+          links: [
+            {
+              id: 'uxu',
+              title: "uxu.pl",
+              linkPath: "https://www.uxu.pl",
+            },
+            {
+              id: 'polskidev',
+              title: "polski.dev",
+              linkPath: "https://www.polski.dev",
+            },
+            {
+              id: 'wTrasie',
+              title: "wtrasie.pl",
+              linkPath: "https://www.wtrasie.pl",
+            },
+          ]
+        },
+        {
+          header: "Informacje prawne",
+          id: "lawsInformation",
+          links: [
+            {
+              id: 'statute',
+              title: "Regulamin",
+              linkPath: "/"
+            },
+            {
+              id: 'privacyPolicy',
+              title: "Polityka Prywatności",
+              linkPath: "/"
+            }
+          ]
+        }
+      ],
     }
   }
 };
