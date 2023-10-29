372b73c7b7628bc8ce662f1a85533252fd8b80ce
Paweł Niedźwiecki <hello@uxu.pl>
2023-10-29 16:22:43 +0100

    fix: name headers && style

--- packages/utils/server/get/getOSInfoFromRequest.ts
+++ packages/utils/server/get/getOSInfoFromRequest.ts
@@ -3,9 +3,9 @@ import type { NextPageContext } from 'next';
 import type { DocumentContext } from "next/document";
 
 export function getOSInfoFromRequest(ctx: NextPageContext | DocumentContext): SiteConfigProps['osInfo'] {
-  const isWindows: boolean = ctx.req?.headers['x-is-windows'] === 'true';
-  const isLinux: boolean = ctx.req?.headers['x-is-linux'] === 'true';
-  const isMacOS: boolean = ctx.req?.headers['x-is-macos'] === 'true';
+  const isWindows: boolean = ctx.req?.headers['uxu-iswindows'] === 'true';
+  const isLinux: boolean = ctx.req?.headers['uxu-islinux'] === 'true';
+  const isMacOS: boolean = ctx.req?.headers['uxu-ismacos'] === 'true';
 
   return {
     isWindows,
