932a8fb6a2c10e49fd4bb81273f15db03c1712ba
Paweł Niedźwiecki <hello@uxu.pl>
2023-11-26 11:17:04 +0100

    feat: add serverActions

--- apps/getreturn/next.config.js
+++ apps/getreturn/next.config.js
@@ -4,4 +4,10 @@ module.exports = {
   images: {
     domains: ['getreturn.s3.eu-central-1.amazonaws.com'],
   },
+  experimental: {
+    serverActions: {
+      allowedForwardedHosts: ['localhost', 'getreturn.pl'],
+      allowedOrigins: ['http://localhost', 'https://www.getreturn.pl']
+    },
+  }
 };
