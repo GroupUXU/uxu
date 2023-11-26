c23434c01b7f2bb180114bd5251b54d5dc92ceef
Paweł Niedźwiecki <hello@uxu.pl>
2023-11-26 11:20:46 +0100

    feat: remove experimental

--- apps/getreturn/next.config.js
+++ apps/getreturn/next.config.js
@@ -4,10 +4,4 @@ module.exports = {
   images: {
     domains: ['getreturn.s3.eu-central-1.amazonaws.com'],
   },
-  experimental: {
-    serverActions: {
-      allowedForwardedHosts: ['localhost', 'getreturn.pl', 'vercel.app'],
-      allowedOrigins: ['http://localhost', 'https://www.getreturn.pl', 'https://vercel.app']
-    },
-  }
 };
