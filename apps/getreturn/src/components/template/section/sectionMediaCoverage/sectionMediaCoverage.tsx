52a47770793d7c7e5ea04b168d287ee1043cf552
Paweł Niedźwiecki <hello@uxu.pl>
2023-12-03 07:56:03 +0100

    fix: SectionMediaCoverage

--- apps/getreturn/src/components/template/section/sectionMediaCoverage/sectionMediaCoverage.tsx
+++ apps/getreturn/src/components/template/section/sectionMediaCoverage/sectionMediaCoverage.tsx
@@ -10,8 +10,7 @@ export function SectionMediaCoverage() {
             <ColorShiftTextAnimator header={header} description={description}/>
             <div className={styles.wrapperButtons}>
                 <Link className="btn primary large" href="#" title="Sprawdź możliwy zwrot">{buttonLeft}</Link>
-                <Link className="btn special large" href="#" title="Jak to działa ?"><span/><span/><span/>{buttonRight}
-                </Link>
+                <Link className="btn special large" href="#" title="Jak to działa ?"><span/><span/><span/>{buttonRight}</Link>
             </div>
             <h4 className={styles.headerPressCoverageLinks}>PISALI O NAS</h4>
             <PressCoverageLinks coverageLinks={coverageLinks} />
