d2beee623ad2f36944de05f8c2189c4865fc5f06
Paweł Niedźwiecki <hello@uxu.pl>
2023-12-04 22:01:31 +0100

    feat: add article for page home

--- apps/getreturn/src/pages/index.tsx
+++ apps/getreturn/src/pages/index.tsx
@@ -3,9 +3,26 @@ import { LayoutFull } from 'design-system/components/layout/layoutFull';
 import { useSeoConfig } from 'design-system/hooks/useSeoConfig';
 import { footerConfig, headerMenuConfig, searchEngineConfig } from '../config';
 import { SectionMediaCoverage } from '../components/template/section/sectionMediaCoverage';
+import {NormalizedCacheObject} from "@apollo/client";
+import {clientGetArticlesQuery, useGetArticlesQuery} from "../gql";
+import {PostShort} from "utils";
+import {adapterArticlesData} from "../utils/adapters";
+import {LeadPostWithList} from "design-system/components/templates/section/leadPostWithList";
 
 function Index(): ReactElement {
     const seo = useSeoConfig({});
+
+    const { data } = useGetArticlesQuery({
+        variables: {
+            pageSize: 12,
+            page: 1,
+            type: ['article']
+        },
+        ssr: true
+    });
+
+    const articlesCopy: Array<PostShort> = data?.articles?.data ? adapterArticlesData(data, "medium") : [];
+    const leadPostWithListData: Array<PostShort> = articlesCopy.slice ( 0, 5 );
 
     return (
         <LayoutFull
@@ -15,8 +32,20 @@ function Index(): ReactElement {
             seo={seo}
         >
            <SectionMediaCoverage />
+            <h4 className="headerSection">NA BLOGU</h4>
+            <LeadPostWithList posts={leadPostWithListData}/>
+
         </LayoutFull>
     );
 };
+
+export async function getServerSideProps(): Promise<{props: { initialApolloState: NormalizedCacheObject }}> {
+    const { apolloClient } = await clientGetArticlesQuery({ variables: { pageSize: 12, page: 1 }})
+    return {
+        props: {
+            initialApolloState: apolloClient.extract()
+        },
+    };
+}
 
 export default Index;
