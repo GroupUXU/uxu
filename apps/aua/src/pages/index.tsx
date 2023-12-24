7d98541d0caf31eada7810686cad181b76492e36
Paweł Niedźwiecki <hello@uxu.pl>
2023-12-24 23:54:50 +0100

    refactor: index aua.pl

--- apps/aua/src/pages/index.tsx
+++ apps/aua/src/pages/index.tsx
@@ -1,6 +1,8 @@
 import type {ReactElement} from 'react';
 import {LayoutFull} from 'design-system/components/layout/layoutFull';
 import {useSeoConfig} from 'design-system/hooks/useSeoConfig';
+import {LeadPostWithList} from "design-system/components/templates/section/leadPostWithList";
+import {SectionWithCircle} from "design-system/components/templates/section/sectionWithCircle";
 import {footerConfig, headerMenuConfig, searchEngineConfig} from '../config';
 import {SectionMediaCoverage} from '../components/template/section/sectionMediaCoverage';
 import {SectionCustomerFaq} from '../components/template/section/sectionCustomerFaq';
@@ -8,8 +10,7 @@ import {NormalizedCacheObject} from "@apollo/client";
 import {clientGetArticlesQuery, useGetArticlesQuery} from "../gql";
 import {PostShort} from "utils";
 import {adapterArticlesData} from "../utils/adapters";
-import {LeadPostWithList} from "design-system/components/templates/section/leadPostWithList";
-import {SectionWithCircle} from "design-system/components/templates/section/sectionWithCircle";
+import {dataWhatItWorks} from "../consts";
 
 function Index(): ReactElement {
     const seo = useSeoConfig({});
@@ -37,78 +38,26 @@ function Index(): ReactElement {
             <h4 className="headerSection">JAK TO DZIAŁA ?</h4>
             <div style={{position: "relative"}}>
                 <div style={{position: "absolute", right: 0, height: "100%", paddingTop: "35rem"}}>
-                    <video src="/video/samojlik.mp4" width="350px" style={{position: 'sticky', top: "8rem"}}
+                    <video src="/video/test.mp4" width="350px" style={{position: 'sticky', top: "8rem"}}
                            preload="auto" autoPlay loop playsInline>
                         <track kind="captions" src="/video/test.mp4" srcLang="pl" label="polish" default/>
                     </video>
                 </div>
-                <SectionWithCircle
-                    inCircle="1"
-                    header="Analiza"
-                    color="var(--uxu-gradient-blue-tell)"
-                >
-                    <SectionCustomerFaq
-                        header="Analiza umowy pod kątem sankcji darmowego kredytu"
-                        description="Sankcja darmowego kredytu, zgodnie z Art. 45. Kodeksu Cywilnego, umożliwia zwrot odsetek z umowy kredytowej. Nasz proces analizy polega na bezpłatnym badaniu, dokonywanym przez prawników w ciągu 72 godzin, przesłanej umowy kredytowej, aby ustalić, czy przysługuje Ci zwrot w ramach sankcji darmowego kredytu."
-                        collapse={[
-                            {
-                                header: "Ile kosztuje analiza umowy ?",
-                                description: "Sankcja darmowego kredytu, zgodnie z Art. 45. Kodeksu Cywilnego, umożliwia zwrot odsetek z umowy kredytowej."
-                            },
-                            {
-                                header: "Czy mogę mieć jakieś problemy z bankiem ?",
-                                description: "Sankcja darmowego kredytu, zgodnie z Art. 45. Kodeksu Cywilnego, umożliwia zwrot odsetek z umowy kredytowej."
-                            },
-                            {
-                                header: "Czy jest jakieś ryzyko ?",
-                                description: "Sankcja darmowego kredytu, zgodnie z Art. 45. Kodeksu Cywilnego, umożliwia zwrot odsetek z umowy kredytowej."
-                            }
-                        ]}
-                    />
-
-                </SectionWithCircle>
-                <SectionWithCircle
-                    inCircle="2"
-                    header="Dokumentacja"
-                    color="var(--uxu-gradient-purple-pink)"
-                >
-                    <SectionCustomerFaq
-                        header="Szykujemy dokumentację dla banku"
-                        description="Po analizie umowy o sankcję darmowego kredytu, jeśli zlecasz sprawę naszym prawnikom, przygotowujemy dokumentację do banku o zwrot prowizji. Bank ma 30 dni na decyzję. W razie potrzeby dodatkowej interpretacji prawnej, zwracamy się do sądu. Po pozytywnej decyzji sądowej, bank wypłaca środki w ciągu 2 tygodni. W przypadku negatywnej, kończymy współpracę i informujemy o niemożności skorzystania z sankcji."
-                        collapse={[
-                            {
-                                header: "Ile trwa cały proces ?",
-                                description: "Sankcja darmowego kredytu, zgodnie z Art. 45. Kodeksu Cywilnego, umożliwia zwrot odsetek z umowy kredytowej."
-                            },
-                            {
-                                header: "Kiedy dostanę pieniądze ?",
-                                description: "Sankcja darmowego kredytu, zgodnie z Art. 45. Kodeksu Cywilnego, umożliwia zwrot odsetek z umowy kredytowej."
-                            },
-                            {
-                                header: "Czy będę musiał chodzić po sądach ?",
-                                description: "Sankcja darmowego kredytu, zgodnie z Art. 45. Kodeksu Cywilnego, umożliwia zwrot odsetek z umowy kredytowej."
-                            }
-                        ]}
-                    />
-
-                </SectionWithCircle>
-                <SectionWithCircle
-                    inCircle="3"
-                    header="Wypłata"
-                    color="var(--uxu-gradient-red-amber)"
-                >
-                    <SectionCustomerFaq
-                        header="Wypłata środków"
-                        description="Po otrzymaniu środków z banku, przelewamy na Twoje konto 50% kwoty zwróconej przez bank w ramach sankcji darmowego kredytu w ciągu tygodnia. Druga połowa to nasze wynagrodzenie za przygotowanie dokumentacji i pokrycie ryzyka związanego z kosztami sądowymi. Dodatkowo, 5% naszego zysku przekazujemy na wybrane przez Ciebie schronisko dla zwierząt po wypłacie środków."
-                        collapse={[
-                            {
-                                header: "Co z moimi danymi po wypłacie środków ?",
-                                description: "Sankcja darmowego kredytu, zgodnie z Art. 45. Kodeksu Cywilnego, umożliwia zwrot odsetek z umowy kredytowej."
-                            },
-                        ]}
-                    />
-
-                </SectionWithCircle>
+                {dataWhatItWorks.map((item) => {
+                    return (
+                        <SectionWithCircle
+                            inCircle={item.inCircle}
+                            header={item.header}
+                            color={item.color}
+                        >
+                            <SectionCustomerFaq
+                                header={item.sectionCustomerFaqData.header}
+                                description={item.sectionCustomerFaqData.description}
+                                collapse={item.sectionCustomerFaqData.collapse}
+                            />
+                        </SectionWithCircle>
+                    )
+                })}
             </div>
             <LeadPostWithList posts={leadPostWithListData}/>
         </LayoutFull>
