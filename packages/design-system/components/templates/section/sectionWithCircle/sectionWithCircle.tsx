d50edbb9a7516c64ba2f5acdb36b25ac46f026bd
Paweł Niedźwiecki <hello@uxu.pl>
2023-12-25 20:29:23 +0100

    fix: style

--- packages/design-system/components/templates/section/sectionWithCircle/sectionWithCircle.tsx
+++ packages/design-system/components/templates/section/sectionWithCircle/sectionWithCircle.tsx
@@ -14,6 +14,7 @@ export function SectionWithCircle({inCircle, header, color, children}: SectionWi
                 className={styles.header}
                 style={{
                     background: color,
+                    backgroundClip: 'text',
                     WebkitBackgroundClip: 'text',
                     WebkitTextFillColor: 'transparent'
                 }}
