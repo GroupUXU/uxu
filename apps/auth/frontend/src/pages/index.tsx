f58dac2cb756a350f40c78ce6c30b4095eff048d
Paweł Niedźwiecki <hello@uxu.pl>
2024-01-06 18:34:40 +0100

    fix: auth front

--- apps/auth/frontend/src/pages/index.tsx
+++ apps/auth/frontend/src/pages/index.tsx
@@ -1,12 +1,9 @@
 import type { ReactElement } from 'react';
 import { LayoutListingPost } from 'design-system/components/layout/layoutListingPost/layoutListingPost';
 import { useSeoConfig } from 'design-system/hooks/useSeoConfig';
-import { useSiteConfig } from "design-system/hooks/useSiteConfig";
 import { footerConfig, headerMenuConfig, searchEngineConfig } from '../config';
 
 function Index (): ReactElement {
-  const { config } = useSiteConfig();
-  const isMobile = config.client.platform.isMobile;
   const seo = useSeoConfig ( {} );
 
 
