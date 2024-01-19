617c0dcab4fd139b4a0cee0681c82fa65a0a8e52
Paweł Niedźwiecki <hello@uxu.pl>
2024-01-19 20:56:35 +0100

    refactor: delete PressCoverageLinks in SectionMediaCoverage

--- apps/aua/src/components/template/section/sectionMediaCoverage/sectionMediaCoverage.tsx
+++ apps/aua/src/components/template/section/sectionMediaCoverage/sectionMediaCoverage.tsx
@@ -1,9 +1,8 @@
 import type { ReactElement } from "react";
 import { ColorShiftTextAnimator } from "design-system/components/atoms/colorShiftTextAnimator";
-import { PressCoverageLinks } from "design-system/components/atoms/pressCoverageLinks";
 import { Link } from "design-system/components/atoms/link";
 import styles from './sectionMediaCoverage.module.scss';
-import { header, description, buttonLeft, buttonRight, coverageLinks } from './consts';
+import { header, description, buttonLeft, buttonRight } from './consts';
 
 export function SectionMediaCoverage(): ReactElement {
     return (
@@ -13,7 +12,7 @@ export function SectionMediaCoverage(): ReactElement {
                 <Link className="btn primary large" href="#start" title="Sprawdź możliwy zwrot">{buttonLeft}</Link>
                 <Link className="btn special large" href="#analize" title="Jak to działa ?"><span/><span/><span/>{buttonRight}</Link>
             </div>
-            <h4 className="headerSection">PISALI O NAS</h4>
-            <PressCoverageLinks coverageLinks={coverageLinks} />
+            {/*<h4 className="headerSection">PISALI O NAS</h4>*/}
+            {/*<PressCoverageLinks coverageLinks={coverageLinks} />*/}
         </section>)
 }
