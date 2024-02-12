5083e3892e5f203838f0c39aa6ea3650bfc4c2a0
pawel <hello@uxu.pl>
2024-02-12 23:47:17 +0100

    feat: update /next.config

--- apps/polskidev/frontend/next.config.js
+++ apps/polskidev/frontend/next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   transpilePackages: ["design-system"],
   images: {
-    domains: ['wtrasiepl.s3.eu-west-1.amazonaws.com'],
+    domains: ['polskidev.s3.eu-central-1.amazonaws.com'],
   },
 };
