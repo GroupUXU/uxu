bfac7c11a950d6d1805fa44400e2ddd3834ae2a0
pawel <hello@uxu.pl>
2024-03-08 22:17:37 +0100

    fix: style for article

--- apps/wtrasie/next.config.js
+++ apps/wtrasie/next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
   transpilePackages: ["design-system"],
   images: {
     domains: ['wtrasiepl.s3.eu-west-1.amazonaws.com'],
